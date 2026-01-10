df2071215d23bccb226258f460ec455a754c04f5
AidysM <mongush10011979@gmail,com>
2026-01-10 15:37:40 +0300

    added pagination

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import post_list, post_detail, AboutPageView, ContactPageView
+from .views import post_list, post_detail, AboutPageView, ContactPageView, PostListView
 
 
 app_name = 'blog'
@@ -8,8 +8,10 @@ app_name = 'blog'
 
 urlpatterns = [
     # post views
-    path('', post_list, name='post_list'),
-    path('<int:id>/', post_detail, name='post_detail'),
+    # path('', post_list, name='post_list'),
+    path('', PostListView.as_view(), name='post_list'),
+    # path('<int:id>/', post_detail, name='post_detail'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', post_detail, name='post_detail'), 
     path('about/', AboutPageView.as_view(), name='about'),
     path('contact/', ContactPageView.as_view(), name='contact'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,48 @@
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 
 from .models import Post
 
 
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 5
+    template_name = 'home.html'
+
+
 def post_list(request):
-    posts = Post.published.all()
+    pos_list = Post.published.all()
+    paginator = Paginator(pos_list, 5)
+    page_number = request.GET.get('page', 1)
+    try: 
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If page_number is not an integer get the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page_number is out of range get last page of results
+        posts = paginator.page(paginator.num_pages)
 
     return render(
         request, 'home.html', {'posts': posts}
     )
 
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     # 
     post = get_object_or_404(
-        Post,
-        id=id,
-        status=Post.Status.PUBLISHED
+        Post, 
+        status=Post.Status.PUBLISHED, 
+        slug=post, 
+        publish__year=year, 
+        publish__month=month, 
+        publish__day=day
     )
 
     return render(
